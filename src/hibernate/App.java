ea87d62d69ec77879a82a259a306f5fe2a4a48c8
ChristianR <christian.rogowski@yahoo.pl>
2018-03-10 12:02:29 +0100

    ProductRepository - returns data from DB by id

--- src/hibernate/App.java
+++ src/hibernate/App.java
@@ -9,14 +9,39 @@ import hibernate.shop.ProductType;
 
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Product merc = new Product("Mercedes S", ProductType.CAR,
-                new Price(new BigDecimal(40000), new BigDecimal(600000)));
-        ProductRepository.saveProduct(merc);
+        Product mercS = new Product("Mercedes S", ProductType.CAR,
+                new Price(new BigDecimal(40000), new BigDecimal(60000)));
+
+        Product mercA = new Product("Mercedes A", ProductType.CAR,
+                new Price(new BigDecimal(10000), new BigDecimal(20000)));
+
+
+        ProductRepository.saveProduct(mercS);
+        ProductRepository.saveProduct(mercA);
+
+        Optional<Product> product1 = ProductRepository.findOneById(1L);
+        Optional<Product> product2 = ProductRepository.findOneById(2L);
+        Optional<Product> product3 = ProductRepository.findOneById(3L);
+
+        //z Optional get() ale niezalecane bo nie sprawdza czy to null
+        //product1.get().getName();
+        // ==
+        //System.out.println(product1.getName());
+        //System.out.println(product2.getName());
+        //System.out.println(product3.getName());
+
+        //jesli null to zwraca pusty String i nie wywala wyjatku
+        System.out.println();
+        System.out.println("Lista wybranych produktow:");
+        System.out.println(product1.map(p->p.getName()).orElse(("")));
+        System.out.println(product2.map(p->p.getName()).orElse(("")));
+        System.out.println(product3.map(p->p.getName()).orElse(("")));
 
     }
 }
--- src/hibernate/shop/ProductRepository.java
+++ src/hibernate/shop/ProductRepository.java
@@ -3,19 +3,49 @@ package hibernate.shop;
 import hibernate.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 
+import java.util.Optional;
+
 public class ProductRepository {
 
-    public static void saveProduct(Product product){
+    /**
+     * Saves product to DB
+     * @param product
+     */
+    public static void saveProduct(Product product) {
 
-
+        Session session = null;
         try {
-            Session session = HibernateUtil.openSession();
+            session = HibernateUtil.openSession();
             session.save(product);
-            session.close();
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
         }
 
 
+    }
+    /**
+     * Returns product from DB by id
+     * @param id
+     */
+    public static Optional <Product> findOneById(Long id){
+        Session session = null;
+        try {
+            session = HibernateUtil.openSession();
+            Product product = session.find(Product.class, id);
+            return Optional.ofNullable(product);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
