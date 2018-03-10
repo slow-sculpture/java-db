ab3802fec6d935dd34e6ea74b90c022af35a94bc
ChristianR <christian.rogowski@yahoo.pl>
2018-03-10 11:44:54 +0100

    ProductRepository - saves product to DB

--- src/hibernate/App.java
+++ src/hibernate/App.java
@@ -1,10 +1,11 @@
 package hibernate;
 
-import hibernate.hibernate.util.HibernateUtil;
+
 import hibernate.shop.Price;
 import hibernate.shop.Product;
+import hibernate.shop.ProductRepository;
 import hibernate.shop.ProductType;
-import org.hibernate.Session;
+
 
 
 import java.math.BigDecimal;
@@ -13,17 +14,9 @@ import java.math.BigDecimal;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        try {
-            Session session = HibernateUtil.openSession();
-
-            Product merc = new Product("Mercedes S", ProductType.CAR,
-                    new Price(new BigDecimal(50000), new BigDecimal(600000)));
-
-            session.save(merc);
-            session.close();
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
+        Product merc = new Product("Mercedes S", ProductType.CAR,
+                new Price(new BigDecimal(40000), new BigDecimal(600000)));
+        ProductRepository.saveProduct(merc);
 
     }
 }
--- src/hibernate/shop/ProductRepository.java
+++ src/hibernate/shop/ProductRepository.java
@@ -1,8 +1,21 @@
 package hibernate.shop;
 
+import hibernate.hibernate.util.HibernateUtil;
+import org.hibernate.Session;
+
 public class ProductRepository {
 
     public static void saveProduct(Product product){
+
+
+        try {
+            Session session = HibernateUtil.openSession();
+            session.save(product);
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
