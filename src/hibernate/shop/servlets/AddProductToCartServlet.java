4d2157e0fc5b32985db82817ad3bc33256a07dee
ChristianR <christian.rogowski@yahoo.pl>
2018-04-07 16:47:44 +0200

    Servlets: add product to cart servlet

--- src/hibernate/shop/servlets/AddProductToCartServlet.java
+++ src/hibernate/shop/servlets/AddProductToCartServlet.java
@@ -1,20 +1,47 @@
 package hibernate.shop.servlets;
 
+import hibernate.shop.cart.Cart;
+import hibernate.shop.cart.CartDetail;
 import hibernate.shop.cart.CartRepository;
+import hibernate.shop.product.Product;
+import hibernate.shop.product.ProductRepository;
+import hibernate.shop.user.User;
+import hibernate.shop.user.UserRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 public class AddProductToCartServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Long id = 1L;
+        Long userId = 1L;
         String productId = req.getParameter("productId");
         String productAmount = req.getParameter("productAmount");
 
-        CartRepository.findByUserId(id);
+        Optional<Cart> byUserId = CartRepository.findByUserId(userId);
+        if(byUserId.isPresent()){
+
+        }else {
+            Optional<User> byEmail= UserRepository.findByEmail("test@wp.pl");
+            Cart cart = new Cart();
+            cart.setUser(byEmail.get());
+            CartDetail cartDetail = new CartDetail();
+            cartDetail.setAmount(new BigDecimal(productAmount));
+            Optional<Product> oneById = ProductRepository.findOneById(Long.valueOf(productId));
+            cartDetail.setProduct(oneById.get());
+            cartDetail.setPrice(oneById.get().getPrice());
+            cart.addCartDetail(cartDetail);
+
+            CartRepository.saveCart(cart);
+
+
+
+        }
     }
 }
